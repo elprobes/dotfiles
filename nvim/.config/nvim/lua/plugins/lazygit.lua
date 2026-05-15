74b9c2a14de9383470d1c9b3966cd29e494cff56
Pr0b3s <elprobes@gmail.com>
2026-05-15 16:06:21 +0200

    Added lazygit lua plugin:

---
+++ nvim/.config/nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,16 @@
+return {
+    {
+        "kdheepak/lazygit.nvim",
+
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+
+        config = function()
+            vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", {
+                silent = true,
+                noremap = true,
+            })
+        end
+    }
+}
