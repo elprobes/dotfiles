5abcd1a0e7f73fbc563d4bde0c7e39bc58f02981
Pr0b3s <elprobes@gmail.com>
2026-05-26 15:36:29 +0200

    Auto indent
    Added autoindent capabilities to nvim for main programming languages

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,7 +7,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-
 -- =========================================================
 -- OPTIONS
 -- =========================================================
@@ -15,163 +14,151 @@ vim.g.maplocalleader = " "
 -- rende più naturale il mapping jk -> Esc
 vim.opt.timeoutlen = 250
 
-
 -- =========================================================
 -- ESC ERGONOMICO
 -- =========================================================
 
 -- insert mode
 vim.keymap.set("i", "jk", "<Esc>", {
-  desc = "Exit insert mode",
+	desc = "Exit insert mode",
 })
 
 -- terminal mode
 vim.keymap.set("t", "jk", [[<C-\\><C-n>]], {
-  desc = "Exit terminal mode",
+	desc = "Exit terminal mode",
 })
-
 
 -- =========================================================
 -- SAVE / QUIT
 -- =========================================================
 
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", {
-  desc = "Save file",
+	desc = "Save file",
 })
 
 vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", {
-  desc = "Quit window",
+	desc = "Quit window",
 })
 
 vim.keymap.set("n", "<leader>x", "<cmd>x<CR>", {
-  desc = "Save and quit",
+	desc = "Save and quit",
 })
 
 vim.keymap.set("n", "<leader>Q", "<cmd>qa!<CR>", {
-  desc = "Force quit all",
+	desc = "Force quit all",
 })
-
 
 -- =========================================================
 -- CLEAR SEARCH HIGHLIGHT
 -- =========================================================
 
 vim.keymap.set("n", "<leader>h", "<cmd>nohlsearch<CR>", {
-  desc = "Clear search highlight",
+	desc = "Clear search highlight",
 })
-
 
 -- =========================================================
 -- WINDOW NAVIGATION
 -- =========================================================
 
 vim.keymap.set("n", "<C-h>", "<C-w>h", {
-  desc = "Move to left split",
+	desc = "Move to left split",
 })
 
 vim.keymap.set("n", "<C-j>", "<C-w>j", {
-  desc = "Move to lower split",
+	desc = "Move to lower split",
 })
 
 vim.keymap.set("n", "<C-k>", "<C-w>k", {
-  desc = "Move to upper split",
+	desc = "Move to upper split",
 })
 
 vim.keymap.set("n", "<C-l>", "<C-w>l", {
-  desc = "Move to right split",
+	desc = "Move to right split",
 })
-
 
 -- =========================================================
 -- SPLITS
 -- =========================================================
 
 vim.keymap.set("n", "<leader>sv", "<cmd>vsplit<CR>", {
-  desc = "Vertical split",
+	desc = "Vertical split",
 })
 
 vim.keymap.set("n", "<leader>sh", "<cmd>split<CR>", {
-  desc = "Horizontal split",
+	desc = "Horizontal split",
 })
-
 
 -- =========================================================
 -- SPLIT RESIZE
 -- =========================================================
 
 vim.keymap.set("n", "<leader>+", "<cmd>resize +5<CR>", {
-  desc = "Increase split height",
+	desc = "Increase split height",
 })
 
 vim.keymap.set("n", "<leader>-", "<cmd>resize -5<CR>", {
-  desc = "Decrease split height",
+	desc = "Decrease split height",
 })
-
 
 -- =========================================================
 -- BUFFER MANAGEMENT
 -- =========================================================
 
 vim.keymap.set("n", "<leader>bn", "<cmd>bnext<CR>", {
-  desc = "Next buffer",
+	desc = "Next buffer",
 })
 
 vim.keymap.set("n", "<leader>bp", "<cmd>bprevious<CR>", {
-  desc = "Previous buffer",
+	desc = "Previous buffer",
 })
 
 vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>", {
-  desc = "Delete buffer",
+	desc = "Delete buffer",
 })
-
 
 -- =========================================================
 -- BETTER INDENTING
 -- =========================================================
 
 vim.keymap.set("v", "<", "<gv", {
-  desc = "Indent left",
+	desc = "Indent left",
 })
 
 vim.keymap.set("v", ">", ">gv", {
-  desc = "Indent right",
+	desc = "Indent right",
 })
-
 
 -- =========================================================
 -- MOVE LINES
 -- =========================================================
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", {
-  desc = "Move selection down",
+	desc = "Move selection down",
 })
 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", {
-  desc = "Move selection up",
+	desc = "Move selection up",
 })
-
 
 -- =========================================================
 -- SYSTEM CLIPBOARD
 -- =========================================================
 
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', {
-  desc = "Yank to system clipboard",
+	desc = "Yank to system clipboard",
 })
 
 vim.keymap.set("n", "<leader>p", '"+p', {
-  desc = "Paste from system clipboard",
+	desc = "Paste from system clipboard",
 })
-
 
 -- =========================================================
 -- TERMINAL
 -- =========================================================
 
 vim.keymap.set("n", "<leader>tt", "<cmd>terminal<CR>", {
-  desc = "Open terminal",
+	desc = "Open terminal",
 })
-
 
 -- =========================================================
 -- TELESCOPE
@@ -181,71 +168,68 @@ vim.keymap.set("n", "<leader>tt", "<cmd>terminal<CR>", {
 -- =========================================================
 
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>", {
-  desc = "Find files",
+	desc = "Find files",
 })
 
 vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", {
-  desc = "Live grep",
+	desc = "Live grep",
 })
 
 vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", {
-  desc = "Recent files",
+	desc = "Recent files",
 })
 
 vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>", {
-  desc = "Find buffers",
+	desc = "Find buffers",
 })
 
 vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", {
-  desc = "Help tags",
+	desc = "Help tags",
 })
-
 
 -- =========================================================
 -- LSP
 -- =========================================================
 
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, {
-  desc = "Go to definition",
+	desc = "Go to definition",
 })
 
 vim.keymap.set("n", "gr", vim.lsp.buf.references, {
-  desc = "References",
+	desc = "References",
 })
 
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {
-  desc = "Hover documentation",
+	desc = "Hover documentation",
 })
 
 vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, {
-  desc = "Rename symbol",
+	desc = "Rename symbol",
 })
 
 vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, {
-  desc = "Code action",
+	desc = "Code action",
 })
-
 
 -- =========================================================
 -- QUICKFIX / LOCATION LIST
 -- =========================================================
 
 vim.keymap.set("n", "<leader>cn", "<cmd>cnext<CR>", {
-  desc = "Quickfix next",
+	desc = "Quickfix next",
 })
 
 vim.keymap.set("n", "<leader>cp", "<cmd>cprev<CR>", {
-  desc = "Quickfix previous",
+	desc = "Quickfix previous",
 })
 
 vim.keymap.set("n", "<leader>co", "<cmd>copen<CR>", {
-  desc = "Open quickfix",
+	desc = "Open quickfix",
 })
 
 vim.keymap.set("n", "<leader>cc", "<cmd>cclose<CR>", {
-  desc = "Close quickfix",
+	desc = "Close quickfix",
 })
-
 
 -- =========================================================
 -- BETTER SCROLLING
@@ -254,7 +238,6 @@ vim.keymap.set("n", "<leader>cc", "<cmd>cclose<CR>", {
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
-
 -- =========================================================
 -- SEARCH RESULTS CENTERED
 -- =========================================================
@@ -262,15 +245,13 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-
 -- =========================================================
 -- KEEP PASTE BUFFER
 -- =========================================================
 
 vim.keymap.set("x", "<leader>p", [['_dP]], {
-  desc = "Paste without overwriting register",
+	desc = "Paste without overwriting register",
 })
-
 
 -- =========================================================
 -- DISABLE EX MODE
@@ -283,5 +264,16 @@ vim.keymap.set("n", "Q", "<nop>")
 -- =========================================================
 
 vim.keymap.set("n", "<leader>e", "<cmd>Neotree toggle<CR>", {
-  desc = "Toggle file explorer",
+	desc = "Toggle file explorer",
 })
+
+-- =========================================================
+-- NEO-TREE
+-- =========================================================
+vim.keymap.set("n", "<leader>i", function()
+	require("conform").format({
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 500,
+	})
+end, { desc = "Format file" })
---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,27 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+
+			vue = { "prettier" },
+
+			html = { "prettier" },
+			css = { "prettier" },
+
+			json = { "prettier" },
+
+			yaml = { "prettier" },
+
+			markdown = { "prettier" },
+		},
+
+		--format_on_save = {
+		--	timeout_ms = 500,
+		--	lsp_fallback = true,
+		--},
+	},
+}
